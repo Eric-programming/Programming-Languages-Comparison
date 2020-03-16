08bbc9e3f7e21a74f9054763a28aef5b077ee29a
Eric-programming <eric09279@outlook.com>
2020-03-16 14:47:24 -0700

    binary sort

--- 5 Searching/ordered_start.py
+++ 5 Searching/ordered_start.py
@@ -2,28 +2,22 @@
 # this technique uses a binary search
 
 
-items = [6, 8, 19, 20, 23, 41, 49, 53, 56, 87]
-
-def binarysearch(item, itemlist):
-    # get the list size
-    listsize = len(itemlist) - 1
-    # start at the two ends of the list
-    lowerIdx = 0
-    upperIdx = listsize
-
-    while lowerIdx <= upperIdx:
-        pass
-        # TODO: calculate the middle point
+items = [6, 8, 19, 20, 23, 41, 49, 53, 56, 87, 99]
 
 
-        # TODO: if item is found, return the index
-
-
-        # TODO: otherwise get the next midpoint
-
-
-    if lowerIdx > upperIdx:
-        return None
+def binarysearch(value, order_list):
+    start = 0
+    end = len(order_list) - 1
+    if (order_list[end] < value or order_list[start] > value):
+        return "Value doesn't exist"
+    while (start <= end):
+        mid = (start + end) // 2
+        if (order_list[mid] == value):
+            return "The index is %d" % mid
+        if (order_list[mid] > value):
+            end = mid - 1
+        else:
+            start = mid + 1
 
 
 print(binarysearch(23, items))
--- 5 Searching/unordered.py
+++ 5 Searching/unordered.py
@@ -4,8 +4,12 @@
 # declare a list of values to operate on
 items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
 
+
 def find_item(item, itemlist):
-    pass
+    for i in range(0, len(itemlist)):
+        if (itemlist[i] == item):
+            return print("Index is ", i)
+    return None
 
 
 print(find_item(87, items))
